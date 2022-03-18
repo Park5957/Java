0b333bf22a1e7dd6c4e8fef5ae7132716031e1fb
Raminbara <98381523+Park5957@users.noreply.github.com>
2022-03-18 16:42:30 +0900

    Add files via upload

---
+++ Game369.java
@@ -0,0 +1,38 @@
+package ppm;
+
+public class Game369 {
+    public static void main(String[] args) {
+        String a;        
+        int count = 0;
+        for (int i = 1; i < 100; i++) {
+            a = Integer.toString(i);
+            if (i > 9) {
+                int i2 = i / 10;
+                int i3 = i % 10;
+
+                if (i2 == 3 || i2 == 6 || i2 == 9) {
+                    if (i3 == 3 || i3 == 6 || i3 == 9) {
+                        a = "¦¦";
+                    } else {
+                        a = "¦";
+
+                    }
+
+                } else if (i3 == 3 || i3 == 6 || i3 == 9) {
+                    a = "¦";
+                }
+            } else if (i < 10 && i % 3 == 0) {
+                a = "¦";
+            }
+
+            System.out.print(a + "\t");
+            count++;
+
+            if (count % 10 == 0) {
+                System.out.print("\n");
+            }
+
+        }
+
+    }
+}
